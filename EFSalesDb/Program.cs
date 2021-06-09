0b64ca374f73df157128014b8b5373bfa2c44ede
John Atkins <JDAtkins@tql.com>
2021-06-09 14:08:16 -0400

    Add project files.

---
+++ EFSalesDb/Program.cs
@@ -0,0 +1,47 @@
+﻿using EFSalesDb.Models;
+using System;
+using System.Linq;
+
+namespace EFSalesDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new SalesDbContext();
+
+            var customers = context.Customers.ToList();
+
+            var customer = context.Customers.Find(1);
+
+            var cincyCustomers = context.Customers
+                .Where(c => c.City == "Cincinnati").ToList();
+
+            //var newCustomer = new Customer()
+            //{
+            //    Name = "TQL",
+            //    City = "Cincinnati",
+            //    State = "OH",
+            //    Sales = 4000000,
+            //    Active = true
+            //};
+            //context.Customers.Add(newCustomer);
+            //var rowsAffected = context.SaveChanges();
+            //Console.WriteLine($"{rowsAffected} row(s) affected.");
+
+            //var tql = context.Customers.Find(36);
+            //tql.Name = "Total Quality Logistics";
+            //var rowsAffected = context.SaveChanges();
+            //Console.WriteLine($"{rowsAffected} row(s) affected.");
+
+            //var tql = context.Customers.Find(36);
+            //context.Customers.Remove(tql);
+            //var rowsAffected = context.SaveChanges();
+            //Console.WriteLine($"{rowsAffected} row(s) affected.");
+
+            var orders = context.Orders.ToList();
+
+
+        }
+    }
+}
